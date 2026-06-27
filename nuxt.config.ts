8db2d9aaf7fe10a13053bd58e6d3fadda3d764b2
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-06-27 22:53:06 +0200

    build: add @nuxt/image and approve sharp builds

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ const DEFAULT_BASE_URL = 'https://partnerapi.funda.nl/feeds/Aanbod.svc/json'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxt/image'],
 
   css: ['~/assets/css/main.css'],
   vite: {
