a44e2f28a352d48a33913f98be9979b5ce119264
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-06-28 13:40:41 +0200

    feat(components): add CommonPrice with null price handling

---
+++ app/components/common/Price.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+defineProps<{ price: number | null }>()
+</script>
+
+<template>
+  <span v-if="price" class="font-semibold truncate"> € {{ price.toLocaleString('nl-NL') }} k.k. </span>
+  <span v-else class="text-neutral-600 truncate"> Price on request </span>
+</template>
