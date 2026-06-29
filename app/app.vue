7b04f49297dd9daf490e77d5e6102fa16e1e60de
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-06-29 11:17:35 +0200

    feat(layout): extract app shell to default layout

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="flex min-h-screen flex-col">
-    <AppHeader />
-
-    <main class="flex-auto mx-auto container">
-      <NuxtPage />
-    </main>
-
-    <AppFooter />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import AppFooter from '~/components/common/AppFooter.vue'
+import AppHeader from '~/components/common/AppHeader.vue'
+</script>
+
+<template>
+  <div class="grid min-h-screen grid-rows-[auto_1fr_auto]">
+    <AppHeader />
+    <main class="container mx-auto p-4">
+      <slot />
+    </main>
+    <AppFooter />
+  </div>
+</template>
